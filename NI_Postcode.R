9a8f632054b7fe3cb554f78433aaedd5f6b83c7d
Martin Mcparland <l00143723@student.lyit.ie>
2019-03-24 17:47:55 +0000

    Initial commit

--- NI_Postcode.R
+++ NI_Postcode.R
@@ -3,3 +3,11 @@
 # 24/03/2019
 # CA2 - NI Postcodes and crime data
 
+# Read in the NI Postcodes data set, show the structure and first 10 rows of the data frame 
+NIPostcodes <- read.csv("~/NI_Postcode/NIPostcodes.csv", header=FALSE)
+str(NIPostcodes)
+head(NIPostcodes, 10)
+
+# Show the total number and mean missing values of the NIPostcode data
+colSums(is.na(NIPostcodes))
+any(is.na(NIPostcodes))
